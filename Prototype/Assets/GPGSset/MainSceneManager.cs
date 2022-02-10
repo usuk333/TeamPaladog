0645a131770fea3542c46953d17ef99b93550d75
ddkaise <usuk333@gmail.com>
2022-02-10 18:43:15 +0900

    220210
    976543

--- Prototype/Assets/GPGSset/MainSceneManager.cs
+++ Prototype/Assets/GPGSset/MainSceneManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SceneManagement;
+using Firebase.Database;
+using System.IO;
 
 public class MainSceneManager : MonoBehaviour
 {
+    GameCenterManager gameCenterManager;
+
+    private int Lv;
+    private int EXP;
+
+    [SerializeField] private Text TLv;
+    [SerializeField] private Text TEXP;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Loading();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Loading()
+    {
+        /*if(gameCenterManager.userdata != null)
+        {
+            EXP = gameCenterManager.userdata.EXP;
+            Lv = gameCenterManager.userdata.Level;
+
+            TEXP.text = EXP.ToString();
+            TLv.text = Lv.ToString();
+        }*/
     }
 }
