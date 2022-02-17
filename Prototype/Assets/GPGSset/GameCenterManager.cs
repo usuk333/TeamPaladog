2653468eb51c4034e61b28cc0f5a6d41f3d277ad
ddkaise <usuk333@gmail.com>
2022-02-17 18:25:33 +0900

    220217 서정우 (세분화작업)
    GameCenterManager가 수행하던 새로운 유저 데이터 저장, 임시 저장, 로그인, 초기화, 파이어베이스 서버 연결 전부 세분화 중.
    내일 Server와 데이터 인계해야함.

--- Prototype/Assets/GPGSset/GameCenterManager.cs
+++ Prototype/Assets/GPGSset/GameCenterManager.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.UI;
 using Firebase;
 using Firebase.Auth;
+using Firebase.Database;
+
+using Google;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+
 using UnityEngine.SceneManagement;
-using Firebase.Database;
 using System.IO;
+using System.Threading.Tasks;
 
 
 public class GameCenterManager : MonoBehaviour
 {
+    //파이어베이스 인증 관리 객체
+    FirebaseAuth auth = null;
+
+    //사용자 계정
+    FirebaseUser user = null;
+
+    //로그인 선택 화면
+    public GameObject LoginPanel;
+
+    //임시 로딩 패널
+    public GameObject LoadingPanel;
+
+    // 닉네임 설정 패널
+    public GameObject NicknamePanel;
+
+    //기기 연동이 되어 있는 상태인지 체크하는 bool 변수
+    private bool signedIn = false;
+
     public string FireBaseId = string.Empty;
-    FirebaseAuth auth;      //파이어베이스 인증 관리 객체
 
     public DatabaseReference reference;        //데이터를 쓰기위한 reference
 
@@ -22,18 +44,33 @@ public class GameCenterManager : MonoBehaviour
 
     //User user;
 
+    // 임시저장용 클래스
+    private User.userLoginData.LoginType tempLoginType = User.userLoginData.LoginType.None;
+    private string tempemail = string.Empty;
+    private string temppw = string.Empty;
+
 
     public bool StartTouch;
     [SerializeField] private Text StartText;
-    [SerializeField] private GameObject LoginPanel;
 
     public UserData userdata;
 
-    void Awake()
+    private void Awake()
     {
+        //Auth용 instance 초기화
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        //유저의 로그인 정보에 어떤 변경이 생기면 실행되게 이벤트를 걸어준다.
+        auth.StateChanged += AuthStateChanged;
+        //AuthStateChanged(this, null);
+
+        //로그인 로딩 패널 비활성
+        LoginPanel.SetActive(false);
+        LoadingPanel.SetActive(false);
+
+        //Firebase 경로및 reference 초기화
         FirebaseDatabase.GetInstance("https://acrobatgames-f9ba6-default-rtdb.firebaseio.com/");
         reference = FirebaseDatabase.DefaultInstance.RootReference;
-        
     }
     void Start()
     {
@@ -81,6 +118,68 @@ public class GameCenterManager : MonoBehaviour
                 }
             }
         });
+    }
+
+    //계정에 어떠한 변경이 발생 시 진행
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if(auth.CurrentUser != user)
+        {
+            //연동된 계정과 기기의 계정이 같다면 true 리턴
+            signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+
+            if(!signedIn && user != null)
+            {
+                Debug.Log("Signed out" + user.UserId);
+            }
+
+            user = auth.CurrentUser;
+
+            if (signedIn)
+            {
+                Debug.Log("Signed in" + user.UserId);
+            }
+        }
+    }
+
+    //로그인 선택 패널을 열며 로그인한 user가 있는지 확인한다
+    //없으면 계정 생성 시작
+    public void LoginCheck()
+    {
+        if (!signedIn)
+        {
+            LoginPanel.SetActive(true);
+        }
+        else
+        {
+            StartCoroutine(CurrentUserDataGet());
+        }
+    }
+
+    //기존 유저 정보 서버에서 가져온다
+    public IEnumerator CurrentUserDataGet()
+    {
+        LoadingPanel.SetActive(true);
+        /*
+        // 유저 정보
+        User.Instance.GetUserData(auth.CurrentUser.UserId, new System.Action(() => {
+            Debug.Log("유저 정보 로드 완료!");
+            // 유저 인벤 정보
+            User.Instance.GetUserInven(auth.CurrentUser.UserId, new System.Action(() => {
+                // 다음 씬으로 넘긴다.
+                NextSecne();
+            }));
+        }));
+        */
+        yield return null;
+    }
+
+    //게임메인씬으로 넘어감
+    public void NextScene()
+    {
+        Debug.Log("GameScene으로");
+
+        SceneManager.LoadSceneAsync(1);
     }
 
     private void Update()
@@ -138,7 +237,7 @@ public class GameCenterManager : MonoBehaviour
         }
     }
 
-    public void GoogleLogin()
+    public void GoogleLoginv()
     {
         if (!Social.localUser.authenticated)
         {
@@ -147,7 +246,7 @@ public class GameCenterManager : MonoBehaviour
                 if (success) // 성공하면
                 {
                     Debug.Log("TryFirebaseLogin 함수 실행");
-                    StartCoroutine(TryFirebaseLogin());
+                    //StartCoroutine(TryFirebaseLogin());
                 }
                 else // 실패하면
                 {
@@ -167,9 +266,10 @@ public class GameCenterManager : MonoBehaviour
         }
     }
 
+    //익명 로그인
     public void GuestLogin()
     {
-        auth = FirebaseAuth.DefaultInstance;
+        //LoadingPanel.SetActive(true);
 
         auth.SignInAnonymouslyAsync().ContinueWith(task => {
             if (task.IsCanceled)
@@ -185,16 +285,176 @@ public class GameCenterManager : MonoBehaviour
             Debug.Log("게스트 로그인 완료");
 
             FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
             Usersid = newUser.UserId;
             //Debug.Log("writeNewUser 함수 발동");
             //writeNewUser(newUser.UserId);
-            
+
             Debug.Log("UIDRigister 시작");
             UIDRigister(newUser.UserId, "Guest");        //기기 저장소 확인하는 if조건문 만들어주는게 좋을듯
             InitializeFirebase();
+        });
 
+        //신규 유저 데이터 임시저장
+        userDataInit();
+        userDataTempSave(User.userLoginData.LoginType.anony);
+
+        //LoadingPanel.SetActive(false);
+    }
+
+    //구글 로그인
+    public void GoogleLogin()
+    {
+        //LoadingPanel.SetActive(true);
+
+        try
+        {
+            //구글 로그인 처리 부분
+            //구글 로그인 팝업창이 꺼지면 실행될 콜백함수를 선언한다.
+            TryFirebaseLogin(new System.Action<bool>((bool chk) =>
+            {
+                if (chk)
+                {
+                    //신규 유저 데이터 임시 저장
+                    userDataInit();
+                    userDataTempSave(User.userLoginData.LoginType.google);
+
+                    //LoadingPanel.SetActive(false);
+                    //닉네임 창 켜주기
+                    //NicknamePanel.SetActive(true);
+                }
+                else
+                {
+                    LoadingPanel.SetActive(false);
+                }
+            }));
+        }
+        catch (System.Exception err)
+        {
+            Debug.LogError(err);
+            LoadingPanel.SetActive(false);
+        }
+    }
+
+
+    //구글 로그인 구동
+    public IEnumerator TryFirebaseLogin(System.Action<bool> callback)
+    {
+        while (string.IsNullOrEmpty(((PlayGamesLocalUser)Social.localUser).GetIdToken()))
+            yield return null;
+
+        string idToken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();   //id토큰가져오기
+        string accessToken = null;
+
+        Credential credential = GoogleAuthProvider.GetCredential(idToken, accessToken);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => { //ContinueWithOnMainThread
+            if (task.IsCanceled)        //실패경우
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                callback(false);
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                callback(false);
+                return;
+            }
+
+            user = task.Result;     //newUser에 task정보 저장.
             Debug.LogFormat("User signed in successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
+                user.DisplayName, user.UserId);
+
+            callback(true);
+
+            Usersid = user.UserId;
+
+            UIDRigister(user.UserId, "Google");
+            InitializeFirebase();
+        });
+    }
+
+    //유저 닉네임 입력 시작
+    private void InsertNewUserData()
+    {
+        var nickname = NicknamePanel.transform.Find("InputField").Find("NickNameInput")
+            .GetComponent<Text>().text;
+
+        if(nickname.Length > 0)
+        {
+            //신규 유저 데이터 입력
+            loginDataSave(tempLoginType, nickname, tempemail, temppw);
+        }
+        else
+        {
+            Debug.Log("별명을 입력해주세요");
+            return;
+        }
+    }
+
+    // 유저 임시 데이터 초기화
+    private void userDataInit()
+    {
+        tempLoginType = User.userLoginData.LoginType.None;
+        tempemail = string.Empty;
+        temppw = string.Empty;
+    }
+
+    // 유저 데이터 임시 저장
+    private void userDataTempSave(User.userLoginData.LoginType loginType, string email = null, string pw = null)
+    {
+        tempLoginType = loginType;
+        tempemail = email;
+        temppw = pw;
+    }
+
+    // 신규 유저 데이터 입력
+    private void loginDataSave(User.userLoginData.LoginType loginType, string nickname = null,
+        string email = null, string pw = null)
+    {
+        // 유저 데이터
+        var newUser = new User.userLoginData();
+
+        // DB에 저장될 유저데이터 초기화
+        newUser.loginType = tempLoginType;
+        newUser.nickname = nickname;
+        newUser.uid = user.UserId;
+        newUser.email = email;
+        newUser.pw = pw;
+        newUser.deviceModel = SystemInfo.deviceModel;
+        newUser.deviceName = SystemInfo.deviceName;
+        newUser.deviceType = SystemInfo.deviceType;
+        newUser.deviceOS = SystemInfo.operatingSystem;
+        newUser.createDate = auth.CurrentUser.Metadata.CreationTimestamp;
+        string json = JsonUtility.ToJson(newUser);
+
+
+        // 코인 저장용
+        var newUserInventory = new User.userGoodsData();
+
+        // DB에 저장될 유저데이터 초기화
+        newUserInventory.uid = user.UserId;
+        newUserInventory.coin = 0;
+
+        LoadingPanel.SetActive(true);
+        LoginPanel.SetActive(false);
+        NicknamePanel.SetActive(false);
+
+        // 위에 정리한 json을 서버에 보내 DB에 저장한다.
+        // 새로운 유저에 대한 데이터를 DB에 보낸다.
+        Server.Instance.NewUserJsonDBSave(json, () => {
+            // DB에 저장 후 디바이스 user정보에도 저장한다.
+            User.Instance.mainUser = newUser;
+
+            // 새로운 유저의 인벤토리 데이터를 DB에 보낸다.
+            Server.Instance.NewUserInventoryJsonDBSave(JsonUtility.ToJson(newUserInventory), () => {
+                // DB에 저장 후 디바이스 user정보에도 저장한다.
+                User.Instance.mainInventory = newUserInventory;
+                // 다음씬으로 이동
+                NextSecne();
+            });
         });
     }
 
@@ -315,38 +575,6 @@ public class GameCenterManager : MonoBehaviour
         public bool Specialist_Unlock;
     }
 
-    public IEnumerator TryFirebaseLogin()
-    {
-        while (string.IsNullOrEmpty(((PlayGamesLocalUser)Social.localUser).GetIdToken()))
-            yield return null;
-        string idToken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();   //id토큰가져오기
-        string accessToken = null;
-
-        auth = FirebaseAuth.DefaultInstance;
-        Credential credential = GoogleAuthProvider.GetCredential(idToken, accessToken);
-        auth.SignInWithCredentialAsync(credential).ContinueWith(task => { //ContinueWithOnMainThread
-            if (task.IsCanceled)        //실패경우
-            {
-                Debug.LogError("SignInWithCredentialAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
-                return;     
-            }
-
-            FirebaseUser newUser = task.Result;     //newUser에 task정보 저장.
-
-            Usersid = newUser.UserId;
-
-            UIDRigister(newUser.UserId, "Google");
-            InitializeFirebase();
-
-            Debug.LogFormat("User signed in successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
-        });
-    }
 
     private void InitializeFirebase()
     {
---
+++ Prototype/Assets/GPGSset/User.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    // 스레드에 안전한 싱글톤 선언
+    private static bool shuttingDown = false;
+    private static object Lock = new object();
+    private static User instance;
+
+    public static User Instance
+    {
+        get
+        {
+            if (shuttingDown)
+            {
+                Debug.LogWarning("User Instance already destroyed. return null");
+                return null;
+            }
+
+            lock (Lock)
+            {
+                if (instance == null)
+                {
+                    instance = (User)FindObjectOfType(typeof(User));
+
+                    if (instance == null)
+                    {
+                        var userObject = new GameObject();
+                        instance = userObject.AddComponent<User>();
+                        userObject.name = "ServerManager";
+
+                        DontDestroyOnLoad(userObject);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // 게임에서 사용할 유저 정보를 저장해두는 곳
+    public userLoginData mainUser;
+    // 유저의 인벤토리
+    public userGoodsData mainInventory;
+
+    // 유저 정보 클래스
+    [System.Serializable]
+    public class userLoginData
+    {
+        public enum LoginType
+        {
+            None = 0,
+            anony = 1,
+            email = 2,
+            google = 3
+        }
+        public string nickname;
+        public LoginType loginType;
+        public string uid;
+        public string email;
+        public string pw;
+        public string deviceModel;
+        public string deviceName;
+        public DeviceType deviceType;
+        public string deviceOS;
+        public ulong createDate;
+    }
+
+    // 유저 재화 보유 클래스
+    [System.Serializable]
+    public class userGoodsData
+    {
+        public string uid;
+        public int coin;
+    }
+
+    // 유저 영수증 정보
+    [System.Serializable]
+    public class userReceipt
+    {
+        public string index;
+        public string uid;
+        public string transaction_id;
+        public string product_id;
+        public string platform;
+        public string price;
+        public string date;
+        public string givecheck;
+    }
+
+
+    // 서버에서 유저 데이터 받아오기
+    public void GetUserData(string uid, System.Action callback)
+    {
+        Server.Instance.GetUserDataDB(uid, callback);
+    }
+
+    // 서버에서 유저 인벤 정보 받아오기
+    public void GetUserInven(string uid, System.Action callback)
+    {
+        Server.Instance.GetUserInvenDB(uid, callback);
+    }
+}
